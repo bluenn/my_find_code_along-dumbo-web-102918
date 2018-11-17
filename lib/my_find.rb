b1844fb141a95eabe8a537a93c0e5f5ed08539c9
a lan <bluenowte97@gmail.com>
2018-11-17 23:40:27 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -13,8 +13,8 @@ def my_find(collection)
 
 #you pass the block argument when you call it! the yield is in the definition
 collection = (1..100).to_a
-my_find(collection) {|1| 1 % 3 == 0 && 1 % 5 == 0 }
+my_find(collection) {|i| i % 3 == 0 && i % 5 == 0 }
   #=> false
-#In this example, ruby will send false
+#In this example, ruby will send 15
 #(the return value of the block) back to the
-#my_find method because 1 % 3 == 0 and 1 % 5 == 0 evaluates to false.
+#my_find method because i % 3 and i % 5, first one true is 15
